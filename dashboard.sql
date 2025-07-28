defbca26ae6b879abfb062b499a77d325ea53fd2
EvgenyPerevera <149911979+EvgenyPerevera@users.noreply.github.com>
2025-07-28 16:09:43 +0300

    Add files via upload

--- dashboard.sql
+++ dashboard.sql
@@ -10,6 +10,26 @@ SELECT
 FROM sessions
 GROUP BY v_date;
 
+--Количество уникальных пользователей для разных каналов:
+SELECT
+  LOWER(source) AS utm_source,
+  COUNT(DISTINCT visitor_id) AS unique_visitors
+FROM sessions
+GROUP BY LOWER(source)
+ORDER BY unique_visitors DESC;
+
+
+-- Количество уникальных пользователей вк
+SELECT 
+COUNT(DISTINCT visitor_id) AS unique_vk_visitors
+FROM sessions
+WHERE LOWER(source) = 'vk';
+
+--Количество уникальных пользователей yandex
+SELECT 
+COUNT(DISTINCT visitor_id) AS unique_yandex_visitors
+FROM sessions
+WHERE LOWER(source) = 'yandex';
 
 
 
@@ -46,10 +66,12 @@ ORDER BY 1, 2, 3;
 
 
 
--- Сколько лидов приходит 
+-- Сколько лидов приходит в общем 
 SELECT 
     count(DISTINCT lead_id) AS leads_count
 FROM leads;
+
+
 
 -- Сколько лидов приходит по дням
 SELECT
@@ -58,13 +80,56 @@ SELECT
 FROM leads
 GROUP BY created_date;
 
+--Сколько лидов приходит для вк и яндекс 
+SELECT
+    LOWER(s.source) AS utm_source,
+    COUNT(DISTINCT l.lead_id) AS leads_count
+FROM leads l
+JOIN sessions s ON l.visitor_id = s.visitor_id
+WHERE LOWER(s.source) IN ('vk', 'yandex')
+GROUP BY LOWER(s.source)
+ORDER BY leads_count DESC;
+
+
+--КОНВЕРСИЯ ИЗ КЛИКА В ЛИД
+
+WITH sessions_with_leads AS (
+    SELECT DISTINCT l.lead_id, l.visitor_id
+    FROM leads l
+    JOIN sessions s
+        ON s.visitor_id = l.visitor_id
+       AND s.visit_date <= l.created_at
+)
+
+SELECT
+    (SELECT COUNT(DISTINCT visitor_id) FROM sessions) AS total_visitors,
+    COUNT(DISTINCT lead_id) AS leads_after_session,
+    ROUND(
+        COUNT(DISTINCT lead_id) * 100.0 / (SELECT COUNT(DISTINCT visitor_id) FROM sessions),
+        2
+    ) AS conversion_to_lead_percent
+FROM leads;
 
 
 
 
+--КОНВЕРСИЯ ИЗ ЛИДА В ОПЛАТУ 
+
+SELECT
+    COUNT(DISTINCT lead_id) AS total_leads,
+    COUNT(DISTINCT lead_id) FILTER (
+        WHERE closing_reason = 'Успешная продажа'
+    ) AS successful_clients,
+    ROUND(
+        COUNT(DISTINCT lead_id) FILTER (
+            WHERE closing_reason = 'Успешная продажа'
+        ) * 100.0 / COUNT(DISTINCT lead_id),
+        2
+    ) AS conversion_to_payment_percent
+FROM leads;
 
 
--- Количество посещений по платным каналам 
+-- Количество посещений по платным каналам (visits_count_source_no_organic)
 SELECT
     to_char(visit_date, 'yyyy-mm-dd')::date AS visit_day,
     source,
@@ -94,7 +159,24 @@ ORDER BY campaign_date, utm_source;
 --ОКУПАЮТСЯ ЛИ КАНАЛЫ?
 
 
--- Расходы по источникам
+--Расходы по всем источникам 
+
+
+
+--Доходы по всем источникам
+
+SELECT
+    LOWER(s.source) AS utm_source,
+    SUM(l.amount) AS total_revenue
+FROM sessions s
+JOIN leads l ON s.visitor_id = l.visitor_id
+WHERE l.status_id = 142 OR l.closing_reason = 'Успешно реализовано'
+GROUP BY LOWER(s.source)
+ORDER BY total_revenue DESC;
+
+
+
+-- Расходы по источникам вк и яндекс (остальные источники органические)
 SELECT 
     utm_source, 
     SUM(daily_spent) AS total_spent
@@ -105,7 +187,7 @@ FROM (
 ) AS ads
 GROUP BY 1;
 
--- Доходы по источникам
+-- Доходы по источникам вк и яндекс
 SELECT
     CASE
         WHEN LOWER(s.source) LIKE '%vk%' THEN 'vk'
@@ -120,17 +202,16 @@ WHERE
 GROUP BY utm_source;
 
 -- roi = (revenue - total_cost) / total_cost * 100%
-roi = (8752676 - 6428804)/6428804 * 100 = 36,15%
+roi общий по платным источникам = (8752676 - 6428804)/6428804 * 100 = 36,15%
 roi vk=(2196731 - 745006)/745006 * 100 = ROI ≈ 194.86%
 roi yandex=(6555945 - 5683798)/5683798 * 100 = ROI ≈ 15.34%
 
 
 
 
-
-/*cpu = total_cost / visitors_count
-cpl = total_cost / leads_count
-cppu = total_cost / purchases_count
+/*cpu (стоимость привлечения 1 пользователя)= total_cost / visitors_count
+cpl (стоимость привлечения одного потенциального клиента (лида))= total_cost / leads_count
+cppu (стоимость привлечения 1 покупателя) = total_cost / purchases_count
 roi = (revenue - total_cost) / total_cost * 100%*/
 
 --CPU
@@ -149,6 +230,9 @@ FROM sessions
 
 6428804/169140=38,03
 
+Для вк = 47,3
+Для яндекс = 307,3
+
 --CPL
 
 SELECT 
@@ -163,7 +247,8 @@ SELECT
     count(DISTINCT lead_id) AS leads_count
 FROM leads;
 
-6428804/1300=4.945 или 4.95k
+Для вк = 12 119
+Для яндекс = 1930
 
 --CPPU
 
@@ -181,7 +266,11 @@ FROM leads
 WHERE closing_reason = 'Успешно реализовано'
    OR status_id = 142;
 
-6428804/205=31.360
+Для вк = 81 197
+Для яндекс = 26 607
+
+
+
 
 -- roi = (revenue - total_cost) / total_cost * 100%
 
@@ -192,7 +281,7 @@ roi yandex=(6555945 - 5683798)/5683798 * 100 = ROI ≈ 15.34%
 
 
 
--- По utm_sourse
+-- Покупатели по каналам
 SELECT
     s.source AS utm_source,
     COUNT(DISTINCT l.lead_id) AS purchases_count
