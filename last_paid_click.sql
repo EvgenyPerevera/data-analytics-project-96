bbb62bbc370b661806630690250a2aca3b4db5a6
EvgenyPerevera <149911979+EvgenyPerevera@users.noreply.github.com>
2025-08-29 11:54:58 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -5,9 +5,13 @@ WITH paid_sessions AS (
         s.source AS utm_source,
         s.medium AS utm_medium,
         s.campaign AS utm_campaign
-    FROM sessions s
-    WHERE LOWER(s.medium) IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+    FROM sessions AS s
+    WHERE
+        LOWER(s.medium) IN (
+            'cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social'
+        )
 ),
+
 ranked_sessions AS (
     SELECT
         l.visitor_id,
@@ -25,33 +29,38 @@ ranked_sessions AS (
             PARTITION BY l.visitor_id, l.lead_id
             ORDER BY ps.visit_date DESC
         ) AS rn
-    FROM leads l
-    LEFT JOIN paid_sessions ps
-        ON l.visitor_id = ps.visitor_id
-        AND ps.visit_date <= l.created_at
+    FROM leads AS l
+    LEFT JOIN paid_sessions AS ps
+        ON
+            l.visitor_id = ps.visitor_id
+            AND l.created_at >= ps.visit_date
 ),
+
 last_paid_clicks AS (
     SELECT *
     FROM ranked_sessions
     WHERE rn = 1
 ),
+
 all_sessions_with_leads AS (
     SELECT
         s.visitor_id,
         s.visit_date,
-        COALESCE(lpc.utm_source, NULL) AS utm_source,
-        COALESCE(lpc.utm_medium, NULL) AS utm_medium,
-        COALESCE(lpc.utm_campaign, NULL) AS utm_campaign,
         lpc.lead_id,
         lpc.created_at,
         lpc.amount,
         lpc.closing_reason,
-        lpc.status_id
-    FROM sessions s
-    LEFT JOIN last_paid_clicks lpc
-        ON s.visitor_id = lpc.visitor_id
-        AND s.visit_date = lpc.visit_date
+        lpc.status_id,
+        COALESCE(lpc.utm_source, NULL) AS utm_source,
+        COALESCE(lpc.utm_medium, NULL) AS utm_medium,
+        COALESCE(lpc.utm_campaign, NULL) AS utm_campaign
+    FROM sessions AS s
+    LEFT JOIN last_paid_clicks AS lpc
+        ON
+            s.visitor_id = lpc.visitor_id
+            AND s.visit_date = lpc.visit_date
 )
+
 SELECT *
 FROM all_sessions_with_leads
 ORDER BY
@@ -59,5 +68,7 @@ ORDER BY
     visit_date ASC,
     utm_source ASC NULLS LAST,
     utm_medium ASC NULLS LAST,
-    utm_campaign ASC NULLS last
+    utm_campaign ASC NULLS LAST
 LIMIT 10;
+
+
