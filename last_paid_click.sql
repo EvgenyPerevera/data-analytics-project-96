af3f6c50c0fd12af09a8444a2f7174e5dcd1b8ad
EvgenyPerevera <149911979+EvgenyPerevera@users.noreply.github.com>
2025-08-29 15:57:30 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,41 +1,44 @@
 SELECT
-  t.visitor_id,
-  t.visit_date,
-  t.utm_source,
-  t.utm_medium,
-  t.utm_campaign,
-  t.lead_id,
-  t.created_at,
-  t.amount,
-  t.closing_reason,
-  t.status_id
+    t.visitor_id,
+    t.visit_date,
+    t.utm_source,
+    t.utm_medium,
+    t.utm_campaign,
+    t.lead_id,
+    t.created_at,
+    t.amount,
+    t.closing_reason,
+    t.status_id
 FROM (
-  SELECT
-    s.visitor_id,
-    s.visit_date::timestamp AS visit_date,
-    lower(s.source)   AS utm_source,
-    lower(s.medium)   AS utm_medium,
-    lower(s.campaign) AS utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id,
-    ROW_NUMBER() OVER (
-      PARTITION BY l.lead_id
-      ORDER BY s.visit_date DESC
-    ) AS rn
-  FROM leads l
-  JOIN sessions s
-    ON s.visitor_id = l.visitor_id
-   AND s.visit_date <= l.created_at
-   AND lower(s.medium) IN ('cpc','cpm','cpa','youtube','cpp','tg','social')
-) t
+    SELECT
+        s.visitor_id,
+        s.visit_date::timestamp AS visit_date,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        lower(s.source) AS utm_source,
+        lower(s.medium) AS utm_medium,
+        lower(s.campaign) AS utm_campaign,
+        row_number() OVER (
+            PARTITION BY l.lead_id
+            ORDER BY s.visit_date DESC
+        ) AS rn
+    FROM leads AS l
+    INNER JOIN sessions AS s
+        ON
+            l.visitor_id = s.visitor_id
+            AND l.created_at >= s.visit_date
+            AND lower(s.medium) IN (
+                'cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social'
+            )
+) AS t
 WHERE t.rn = 1
 ORDER BY
-  t.amount DESC NULLS LAST,
-  t.visit_date ASC,
-  t.utm_source ASC,
-  t.utm_medium ASC,
-  t.utm_campaign ASC
+    t.amount DESC NULLS LAST,
+    t.visit_date ASC,
+    t.utm_source ASC,
+    t.utm_medium ASC,
+    t.utm_campaign ASC
 LIMIT 10;
