968506232d8e53f2932087f2ad3cb676d8444afd
EvgenyPerevera <149911979+EvgenyPerevera@users.noreply.github.com>
2025-08-30 09:47:59 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -54,45 +54,65 @@ INNER JOIN (
 
 /*CPPU (cтоимость 1 покупателя =
 рекламные затраты / количество оплаченных сделок)*/
+WITH p AS (
+    SELECT
+        LOWER(s.source) AS channel,
+        COUNT(DISTINCT l.lead_id) AS purchases_count
+    FROM leads AS l
+    INNER JOIN sessions AS s ON l.visitor_id = s.visitor_id
+    WHERE
+        LOWER(s.source) IN ('vk', 'yandex')
+        AND (l.closing_reason = 'Успешно реализовано' OR l.status_id = 142)
+    GROUP BY
+        LOWER(s.source)
+)
+
 SELECT
     c.channel,
     ROUND(c.total_cost::numeric / NULLIF(p.purchases_count, 0), 2) AS cppu
 FROM (
-    SELECT 'vk' AS channel, SUM(daily_spent) AS total_cost FROM vk_ads
+    SELECT
+        'vk' AS channel,
+        SUM(daily_spent) AS total_cost
+    FROM vk_ads
     UNION ALL
-    SELECT 'yandex' AS channel, SUM(daily_spent) AS total_cost FROM ya_ads
-) c
-JOIN (
-    SELECT LOWER(s.source) AS channel,
-           COUNT(DISTINCT l.lead_id) AS purchases_count
-    FROM leads l
-    JOIN sessions s ON l.visitor_id = s.visitor_id
-    WHERE LOWER(s.source) IN ('vk','yandex')
-      AND (l.closing_reason = 'Успешно реализовано' OR l.status_id = 142)
-    GROUP BY
-        LOWER(s.source)
-) p
-  ON c.channel = p.channel;
-
-  ROI (Return on Investment)
+    SELECT
+        'yandex' AS channel,
+        SUM(daily_spent) AS total_cost
+    FROM ya_ads
+) AS c
+INNER JOIN p
+    ON c.channel = p.channel;
 
 --ROI = (Выручка − Затраты) / Затраты
+WITH r AS (
+    SELECT
+        LOWER(s.source) AS channel,
+        SUM(l.amount) AS revenue
+    FROM leads AS l
+    INNER JOIN sessions AS s ON l.visitor_id = s.visitor_id
+    WHERE
+        LOWER(s.source) IN ('vk', 'yandex')
+        AND (l.closing_reason = 'Успешно реализовано' OR l.status_id = 142)
+    GROUP BY LOWER(s.source)
+)
+
 SELECT
     c.channel,
     ROUND((revenue - c.total_cost)::numeric / NULLIF(c.total_cost, 0), 4) AS roi
 FROM (
-    SELECT 'vk' AS channel, SUM(daily_spent) AS total_cost FROM vk_ads
+    SELECT
+        'vk' AS channel,
+        SUM(daily_spent) AS total_cost
+    FROM vk_ads
     UNION ALL
-    SELECT 'yandex' AS channel, SUM(daily_spent) AS total_cost FROM ya_ads
-) c
-JOIN (
-    SELECT LOWER(s.source) AS channel,
-           SUM(l.amount) AS revenue
-    FROM leads l
-    JOIN sessions s ON l.visitor_id = s.visitor_id
-    WHERE LOWER(s.source) IN ('vk','yandex')
-      AND (l.closing_reason = 'Успешно реализовано' OR l.status_id = 142)
-    GROUP BY LOWER(s.source)
-) r
-  ON c.channel = r.channel;
+    SELECT
+        'yandex' AS channel,
+        SUM(daily_spent) AS total_cost
+    FROM ya_ads
+) AS c
+INNER JOIN r
+    ON c.channel = r.channel;
+
+
 
